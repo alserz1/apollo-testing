465621b3c43659f0a3d02349d187781ff2400ba8
as.zaycev <as.zaycev@tensor.ru>
2021-09-23 13:42:06 +0300

    удалил лишний коммент

--- src/useTypePolicies.ts
+++ src/useTypePolicies.ts
@@ -1,12 +1,5 @@
 import { useApolloClient, InMemoryCache, TypePolicies } from '@apollo/client';
 
-/**
- * чтобы брать из кэша, нужны данные
- * как данные придут, можно записать их через writeQuery в кэш, но перед этим их надо как-то преобразовать
- *
- * обратное преобразование всё ещё хз как. Да и прямое преобразование не сильно проще, дешевле новый бэкенд поднять
- */
-
 export function useTypePolicies(policies: TypePolicies) {
     const client = useApolloClient();
     (client.cache as InMemoryCache).policies.addTypePolicies(policies);
